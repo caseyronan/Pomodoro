8cec4732e238c750e16d2d20542e79f4e93e92b4
Ronan Casey <caseyronan@gmail.com>
2017-04-03 13:03:38 +0700

    initial commit

---
+++ WpfPomodoro/Controller.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WpfPomodoro
+{
+    class Controller : ObservableObject
+    {
+        private Timer _timer;
+        private DateTime _startTime;
+        private string _formattedTime = "00:00";
+        private bool _timerStarted = false;
+
+        /// <summary>
+        /// Code to start the timer when the user presses the Start button.
+        /// </summary>
+        public void Start()
+        {
+            if (!_timerStarted)
+            {
+                _timerStarted = true;
+                _startTime = DateTime.Now;
+                _timer = new Timer(1000); // 1000ms
+                _timer.Elapsed += timer_Elapsed;
+                _timer.Enabled = true;
+            }
+        }
+
+        public void Stop()
+        {
+            // write testable code
+        }
+
+        public void Reset()
+        {
+            // write testable code
+        }
+
+        public string FormattedTime
+        {
+            get { return _formattedTime; }            
+        }
+
+        public void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            TimeSpan timeSinceStart = DateTime.Now - _startTime;
+            _formattedTime = timeSinceStart.ToString(@"mm\:ss");
+            OnPropertyChanged("FormattedTime");
+        }
+    }
+}
---
+++ WpfPomodoro/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfPomodoro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Controller controller { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            controller = new Controller();
+            this.DataContext = controller;
+        }
+
+        private void StartTimer(object sender, RoutedEventArgs e)
+        {
+            controller.Start();
+        }
+
+        private void StopTimer(object sender, RoutedEventArgs e)
+        {
+            controller.Stop();
+        }
+
+        private void ResetTimer(object sender, RoutedEventArgs e)
+        {
+            controller.Reset();
+        }
+    }
+}
